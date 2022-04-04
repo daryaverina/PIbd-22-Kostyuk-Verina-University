83cb7c0894b68101ad69f161b8a39db7eaccf186
lotsoftoads <lotsoftoads@yahoo.com>
2022-04-04 23:13:06 +0400

    поправки в конституцию

--- University/UniversityDatabaseImplement/Models/Student.cs
+++ University/UniversityDatabaseImplement/Models/Student.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UniversityDatabaseImplement.Models
 {
-    // Поставщик
+    // Студент
     public class Student
     {
         // Номер зачетки
