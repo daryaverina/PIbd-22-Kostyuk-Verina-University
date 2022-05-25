e7a907702913d2dafeb78bb963fd933b88d2a59c
d <dashka400g@gmail.com>
2022-05-25 22:35:37 +0400

    chto-to

---
+++ University/UniversityView/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using UniversityBusinessLogic.BusinessLogics;
+using UniversityContracts.BusinessLogicsContracts;
+using UniversityContracts.StorageContracts;
+using UniversityContracts.ViewModels;
+using UniversityContracts.BindingModels;
+using UniversityDatabaseImplement.Implements;
+using UniversityBusinessLogic.OfficePackage;
+using Unity;
+using Unity.Lifetime;
+using System.Windows;
+
+namespace UniversityView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static IUnityContainer container = null;
+        public static CustomerViewModel Customer { get; set; }
+        public static IUnityContainer Container
+        {
+            get
+            {
+                if (container == null)
+                {
+                    container = BuildUnityContainer();
+                }
+                return container;
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var authorizationWindow = Container.Resolve<SignInWindow>();
+            authorizationWindow.ShowDialog();
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var currentContainer = new UnityContainer();
+            currentContainer.RegisterType<ICustomerStorage, CustomerStorage>(new HierarchicalLifetimeManager());
+
+
+            currentContainer.RegisterType<ICustomerLogic, CustomerLogic>(new HierarchicalLifetimeManager());
+
+            return currentContainer;
+        }
+    }
+}
+
---
+++ University/UniversityView/SignUpWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using UniversityContracts.BindingModels;
+using UniversityContracts.BusinessLogicsContracts;
+
+namespace UniversityView
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+        private readonly ICustomerLogic _logic;
+        public SignUpWindow(ICustomerLogic logic)
+        {
+            InitializeComponent();
+            _logic = logic;
+        }
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBoxLogin.Text))
+            {
+                MessageBox.Show("Введите логин - ваша электронная почта", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(TextBoxPassword.Text))
+            {
+                MessageBox.Show("Введите пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(TextBoxPhone.Text))
+            {
+                MessageBox.Show("Введите номер телефона ", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(TextBoxFullName.Text))
+            {
+                MessageBox.Show("Введите полное имя", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                _logic.CreateOrUpdate(new CustomerBindingModel
+                {
+                    Name = TextBoxFullName.Text,
+                    Phone = TextBoxPhone.Text,
+                    Login = TextBoxLogin.Text,
+                    Password = TextBoxPassword.Text,
+                    Email = "default@com"
+                });
+                MessageBox.Show("Вы успешно зарегистрировались", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
